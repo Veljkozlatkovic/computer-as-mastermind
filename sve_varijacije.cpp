5200175f38a95225dca6b475818fc011f8ce05b1
Veljkozlatkovic <149970940+Veljkozlatkovic@users.noreply.github.com>
2024-06-10 20:10:25 +0200

    Update and rename testic.cpp to sve_varijacije.cpp

---
+++ sve_varijacije.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h> // kod uzet sa petlje
+
+using namespace std;
+namespace fs=std::filesystem;
+
+
+void obradi(const vector<int>& varijacija) {
+
+ofstream outputFile("set.txt");
+
+    outputFile.open("set.txt");
+//! ////////////////////////////////////////////////////
+ if(outputFile.is_open()) {
+    cout << "uspesno" << endl;
+    }
+  for (int x : varijacija)
+    cout << x;
+  cout << " ";
+//! /////////////////////////////////////////////////////
+
+for (int x : varijacija)
+    outputFile << x;
+outputFile << ' ';
+outputFile.close();
+//!
+
+
+}
+
+void obradiSveVarijacije(int n, vector<int>& varijacija, int m) {
+  if (m == varijacija.size())
+    obradi(varijacija);
+  else
+    for (int nn = 1; nn <= n; nn++) {
+       varijacija[m] = nn;
+       obradiSveVarijacije(n, varijacija, m+1);
+    }
+}
+
+void obradiSveVarijacije(int k, int n) {
+  vector<int> varijacija(k);
+  obradiSveVarijacije(n, varijacija, 0);
+}
+
+
+int main() {
+  int n = 6, k = 4;
+  obradiSveVarijacije(k, n);
+  return 0;
+}
+
+
+
+outFile.close();
+
+
+}
+*/
